132c354f1d59a63498bc811553f8e9ef997fb050
Hritik <hritkmjth@gmail.com>
2023-02-03 22:35:35 +0545

    Changes in client

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 from Crypto import Random
-from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from CryptoClass import AESCrypt, RSACrypt
 
@@ -18,19 +17,28 @@ def client_program(cl_priv_key, cl_pub_key):
     client_socket.send(cl_pub_key)      #send public key to server
 
     encrypted_session_key = client_socket.recv(256) #receive encrypted session key from server 
+    print(encrypted_session_key)
     rsa = RSACrypt(cl_priv_key)         #Creating an RSA object to decrypt session key
     session_key = rsa.decrypt_sesskey(encrypted_session_key)
     aes = AESCrypt(session_key)         #Creating an AES object to encrypt/decrypt message
 
-    message = input(" -> ")
+    try:
+        message = input("Say Hello to Server -> ")
 
-    while message.lower().strip() != 'exit':
-        en_msg = aes.encrypt(message)
-        client_socket.send(en_msg)
-        encrypted_message = client_socket.recv(1024)
-        decrypted_message = aes.decrypt(encrypted_message)
-        print("Server: " + decrypted_message)
-        message = input("Client -> ")
+        while message.lower().strip() != 'exit':
+            en_msg = aes.encrypt(message)
+            client_socket.send(en_msg)
+            encrypted_message = client_socket.recv(1024)
+            if not encrypted_message:
+                break
+            decrypted_message = aes.decrypt(encrypted_message)
+            print("Server: " + decrypted_message)
+            message = input("Client -> ")
+    except ValueError:
+        print("Server -> Invalid message cannot be sent. Only alphanumeric characters and .,!? are allowed.")
+        pass
+    except KeyboardInterrupt:
+        pass
     
     client_socket.close()
 
